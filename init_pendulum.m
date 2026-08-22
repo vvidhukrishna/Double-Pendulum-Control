50bd920635938364a8c6cee2986acb19bedd4ed8
vvinodkrishnan <vvinodkrishnan@vaishakhsolutions.com>
2026-08-22 21:12:57 +0530

    Added innit file, did visual cleanup, removed magic numbers, plotted velocity and position in scope.

---
+++ init_pendulum.m
@@ -0,0 +1,32 @@
+% Geometry (m)
+rod1_length = 1;
+rod2_length = 0.75;
+
+rod1_radius = 0.05;
+rod2_radius = 0.05;
+
+pin_length = 0.15;
+pin_radius = 0.1;
+
+% Mass Properties (g/cm^3)
+pin_density = 4.6;
+density1 = 2.7;
+density2 = 7.7;
+
+% Initial Conditions (degrees)
+pin1_angle = -35;
+pin2_angle = 175;
+
+pin1_equlibrium = -90;
+pin2_equlibrium = 0;
+
+% Damping
+pin1_damping = 0.025;
+pin2_damping = 0.01;
+
+% Rigid Transformation values (degrees and m)
+R1_angel = 90; % +X
+R1_offset = 0.5; % -Y
+
+R2_angle = 90; % +X
+R2_offset = 0.375; % -Y
